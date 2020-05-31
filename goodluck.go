5a99c6cc181017b31cc6fd8aaff832a49a51c044
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-31 14:50:56 +0530

    Read random fortune

--- goodluck.go
+++ goodluck.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"time"
 )
 
 func randInt(min, max int) int {
@@ -38,4 +42,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	rand.Seed(time.Now().UnixNano())
+	i := randInt(1, len(files))
+	randomFile := files[i]
+
+	file, err := os.Open(randomFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	quotes := string(b)
+
+	quotesSlice := strings.Split(quotes, "%")
+	j := randInt(1, len(quotesSlice))
+
+	fmt.Print(quotesSlice[j])
+
 }
